5413a0351dc74ee5f022e793d12764c04685588c
danielsalim304 <danielsalim304@gmail.com>
2022-11-29 13:42:05 +0700

    Adding Screenshoot

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:photo_view/photo_view.dart';
-import 'package:photo_view/photo_view_gallery.dart';
 import 'package:sidebarx/sidebarx.dart';
 
 class Homepage extends StatefulWidget {
